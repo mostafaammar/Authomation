fff6fa5cac4eb263cb5f1430944f7e8654071574
mostafaammar <mostafaammar@users.noreply.github.com>
2020-02-01 10:10:19 +0200

    Update connecting_cisco_asa_show_interface.py

--- connecting_cisco_asa_show_interface.py
+++ connecting_cisco_asa_show_interface.py
@@ -4,13 +4,7 @@ from netmiko import ConnectHandler
 import getpass
 import textfsm
 import csv
-#
-# constants= open("con.txt",'r')
-# x=constants.readlines()
-# print (type(x))
-# username=str(x[0]).lstrip()
-# host=str(x[1]).lstrip()
-# print (username,host)
+
 
 username=getpass.getpass('please enter username: ')
 host=getpass.getpass('please enter host ip address: ')
@@ -25,7 +19,8 @@ cisco1 = {
 file = open( "show interface.txt" ,"w")
 file.write("besmellah \n")
 net_connect = ConnectHandler(**cisco1)
-print(net_connect.find_prompt())
+net_connect.find_prompt()
+#Enter enable password
 output1 = net_connect.send_command_timing("enable"+"\n")
 pass123 =  getpass.getpass("enter enable password: ")
 
@@ -33,8 +28,12 @@ if "Password:" in output1 :
     output1 = net_connect.send_command_timing(pass123, strip_prompt=False, strip_command=False)
 # to generate the output without --more in cisco asa
 z= net_connect.send_command_timing("terminal pager 0")
+#send the show interface command to asa and get the command output parsed used textfsm library
+#the output is a list of dictionaries
+#each dictionary represents an interface output parsed 
 output2=net_connect.send_command_timing("show interface" , use_textfsm=True)
 
+#transform output to Comma separated file to open with excel
 with open('show_interface_cisco_asa.csv', 'w') as csvfile:
      for items in output2:
         print (items['interface'],items["packets_input"],items["ip_address"])
@@ -42,8 +41,6 @@ with open('show_interface_cisco_asa.csv', 'w') as csvfile:
         fieldnames=['interface',"packets_input","ip_address"]
         x = [items['interface'],items["packets_input"], items["ip_address"]]
         writer.writerow(x)
-print ("type of output2 is ", type(output2))
-print ("type of items is ", type(items))
 
 
 
